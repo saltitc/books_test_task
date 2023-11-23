b03a1c119513284a9a3b9039dce6906f3114ce8e
Ayvaz Izmailov <saltinthecut@gmail.com>
2023-11-23 20:25:53 +0300

    A book model and an API for managing books have been created. Endpoints created: - To get a list of all books. - To obtain information about a specific book. - To create a new book. - To update information about the book. - To Delete a book.

---
+++ library/books/views.py
@@ -0,0 +1,29 @@
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookListView(generics.ListAPIView):
+    """
+    With a POST request, returns a list of all books
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookCreateView(generics.CreateAPIView):
+    """
+    With a POST request, creates a book in the database
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    With a GET request,returns detail information about the book;
+    With a PUT request, updates book information;
+    With a DELETE request, deletes a book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
